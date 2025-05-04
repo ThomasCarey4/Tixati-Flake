a83220f492cbb5d04dec8468ef0da848fa2496a8
ThomasCarey4 <scarey.tcarey@gmail.com>
2025-05-04 21:02:51 +0100

    Some Changes :)

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in
     {
+      programs.nix-ld.enable = true;
+      programs.nix-ld.libraries = with pkgs; [
+        dbus
+      ];
       packages.${system}.tixati = pkgs.stdenv.mkDerivation rec {
         pname = "tixati";
         version = "3.26"; # Update to the correct version if necessary
@@ -26,8 +30,9 @@
           steam-run $out/lib/tixati.binary" > $out/bin/tixati
 
           chmod +x $out/bin/tixati
+          
           cp $src/lib/tixati.binary $out/lib/tixati.binary
-
+          chmod +x $out/lib/tixati.binary
           cp $src/share/icons/hicolor/48x48/apps/tixati.png $out/share/icons/hicolor/48x48/apps/tixati.png
           
           cp $src/share/applications/tixati.desktop $out/share/applications/tixati.desktop
